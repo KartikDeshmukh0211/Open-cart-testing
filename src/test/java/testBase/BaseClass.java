d713197ebe0b47500068f69258ff099770c1767f
Kartik Deshmukh <kartikdeshmukh58@gmail'.com>
2026-03-11 23:39:05 +0530

    crossbrwoser testing done

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -5,9 +5,11 @@ import java.time.Duration;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
+import org.testng.annotations.Parameters;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger; // only import this log4j
 
@@ -24,10 +26,18 @@ public class BaseClass {
     public Logger logger; // import this from log4j.......
 
     @BeforeClass
-    public void setup(){
+    @Parameters({"os", "browser"})
+    public void setup(String os, String br){
         logger = LogManager.getLogger(this.getClass()); // for logger configurationm
         
-        driver = new ChromeDriver();
+        switch(br.toString()){
+            case "chrome" : driver = new ChromeDriver(); break;
+            case "edge" : driver = new EdgeDriver(); break;
+            case "firefox" : driver = new FirefoxDriver(); break;
+            default : System.out.println("Invalid Browser"); return;
+        }
+        
+        // driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
